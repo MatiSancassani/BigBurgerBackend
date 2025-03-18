dd9cb63664d64efd2135be762e3458a7fa5c63fe
Sanca <matiassancassani@gmail.com>
2025-03-18 00:36:31 -0300

    GoogleCloudStorage

--- src/config.js
+++ src/config.js
@@ -15,12 +15,5 @@ const config = {
     JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
 
     MONGODB_URI: ('mongodb+srv://matisancassani:sanca123@cluster0.lcblgku.mongodb.net/BigBurgerDB'),
-
-    type: process.env.GCS_TYPE,
-    project_id: process.env.GCS_PROJECT_ID,
-    private_key_id: process.env.GCS_PRIVATE_KEY_ID,
-    private_key: process.env.GCS_PRIVATE_KEY.replace(/\\n/g, '\n'), // 🔥 Corrige los saltos de línea en la clave
-    client_email: process.env.GCS_CLIENT_EMAIL,
-
 }
 export default config;
